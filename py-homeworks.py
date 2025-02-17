09b463162a91094e332541e52415789f89eda354
NacityBos <163729221+NacityBos@users.noreply.github.com>
2025-02-18 01:09:01 +0300

    Update py-homeworks.py

--- py-homeworks.py
+++ py-homeworks.py
@@ -1,5 +1,5 @@
-
 class Student:
+    students = []
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -7,6 +7,20 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.students.append(self)
+
+    def avg_grade(self):
+        overall_grade = 0
+        grades_count = 0
+        if len(self.grades) == 0:
+            return 0
+        else:
+            for grades in self.grades.values():
+                if len(grades) > 0:
+                    for grade in grades:
+                        overall_grade += grade
+                        grades_count += 1
+            return overall_grade / grades_count
 
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
@@ -15,10 +29,9 @@ class Student:
         return \
             f'Имя: {self.name}'\
             f'Фамилия: {self.surname}'\
-            f'Средняя оценка за домашние задания: {self.surname}' \
+            f'Средняя оценка за домашние задания: {round(self.avg_grade(), 2)}' \
             f'Курсы в процессе изучения: {self.courses_in_progress}' \
             f'Завершенные курсы: {self.finished_courses}'
- \
 
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached \
@@ -29,6 +42,12 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            return self.avg_grade() < other.avg_grade()  # true or false
+        else:
+            return "Ошибка"
 
 
 class Mentor:
@@ -42,13 +61,30 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+        lecturers_list.append(self)
+
+    def avg_grade(self):
+        overall_grade = 0
+        if len(self.grades) > 0:
+            for grade in self.grades:
+                overall_grade += grade
+            return overall_grade / len(self.grades)
+        else:
+            return 0
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.avg_grade() < other.avg_grade()
+        else:
+            return "Ошибка"
 
     def __str__(self):
         return \
             f'Имя: {self.name}'\
             f'Фамилия: {self.surname}' \
-            f'Средняя оценка за лекции: {self.surname}' \
- \
+            f'Средняя оценка за лекции: {self.avg_grade()}'
+
+
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
@@ -62,21 +98,34 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
     def __str__(self):
         return \
             f'Имя: {self.name}'\
             f'Фамилия: {self.surname}'
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+def avg_grade_all_lecturers(lecturers_list, course_name):
+    all_lecturers_avg_grade = 0
+    lecturers_count = 0
+    for lecturer in lecturers_list:
+        if isinstance(lecturer, Lecturer) and course_name in lecturer.courses_attached:
+            all_lecturers_avg_grade += lecturer.avg_grade()
+            lecturers_count += 1
+    if lecturers_count == 0:
+        return 'Ошибка'
+    return round(all_lecturers_avg_grade / lecturers_count, 2)
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+def avg_grade_all_students(students, course_name):
+    all_students_avg_grade = 0
+    students_count = 0
+    for student in students:
+        if isinstance(student, Student) and course_name in student.courses_in_progress:
+            all_students_avg_grade += student.avg_grade()
+            students_count += 1
+    if students_count == 0:
+        return 'Ошибка'
+    return round(all_students_avg_grade / students_count, 2)
 
-print(best_student.grades)
 
